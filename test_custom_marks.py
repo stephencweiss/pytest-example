d1c7a5ca1fe2bd8480ebf12ed1fe004ddc5d1bba
Stephen Weiss <stephen.c.weiss@gmail.com>
2020-06-02 16:28:53 -0500

    feat: custom markers strict

---
+++ test_custom_marks.py
@@ -0,0 +1,16 @@
+import pytest
+from main import increment
+
+@pytest.mark.mark_one()
+def test_increment_one():
+    assert increment(0) == 1
+
+
+@pytest.mark.mark_two()
+def test_increment_two():
+    assert increment(0) == 1
+
+
+@pytest.mark.mark_three()
+def test_increment_three():
+    assert increment(0) == 1
