d06596fa46ed6fd32824cf4235bc55ff1b2df601
Stephen Weiss <stephen.c.weiss@gmail.com>
2020-06-04 13:22:43 -0500

    feat: handle divide errors

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def increment(n):
-  if not isinstance(n, int):
-    raise NonIntegerException
-  return n + 1
+    if not isinstance(n, int):
+        raise NonIntegerException
+    return n + 1
+
 
 def divide(numerator, denominator):
-  return numerator / denominator
+    if(denominator == 0):
+        raise ZeroDenominator
+    return numerator / denominator
+
+
+class ZeroDenominator(Exception):
+    pass
+
 
 class NonIntegerException(Exception):
-  pass
+    pass
